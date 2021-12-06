70a3f9a2cad34a03cef2df77242c61bc3b702f4e
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2021-12-06 15:55:52 -0500

    add get_lat_long(index) -> (f32,f32)

--- src/lib.rs
+++ src/lib.rs
@@ -16,14 +16,14 @@ pub fn unpack_f32(x: u64) -> (f32, f32) {
     (f32::from_bits((x >> 32) as u32), f32::from_bits(x as u32))
 }
 
-// pub fn unpack_to_coords(x: u64) -> (f32, f32) {
-//     let (lat, lon) = unpack(x);
-//     (lat as f32 * 1e-7, lon as f32 * 1e-7)
-// }
-
 pub trait Cache {
     fn get_value(&self, index: usize) -> u64;
     fn set_value(&mut self, index: usize, value: u64);
+
+    fn get_lat_long(&self, index: usize) -> (f32, f32) {
+        let (lat, lon) = unpack_f32(self.get_value(index));
+        (lat as f32 * 1e-7, lon as f32 * 1e-7)
+    }
 
     fn set_value_f32(&mut self, index: usize, lat: f32, lon: f32) {
         self.set_value(index, pack_f32(lat, lon))
